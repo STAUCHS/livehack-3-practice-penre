5eb1d505baadba85cc64d19fd58c1bdb46811eee
penre <tianyou.zhang25@ycdsbk12.ca>
2024-04-25 18:43:22 -0400

    finished 1st row

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main extends ConsoleProgram {
   public void run() {
-    System.out.print(countCode("codexxcode"));
+    System.out.print(countHi("nihilihi"));
   }
   /**
    * make a program that repeats everyletter in a string 
@@ -18,16 +18,120 @@ class Main extends ConsoleProgram {
     return result;
   }
   
-
+  /**
+   * Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+   * @param str
+   * @return
+   */
   public int countCode(String str){
     int result = 0;
-    int count;
 
-    for (int i = 0; i < str.length(); i++){
-      for (count = str.indexOf("code"); count < str.length(); count++){
-        result += count;
+    for (int i = 0; i < str.length() - 3; i++){
+      if (str.substring(i , i + 2).equals("co") && str.substring(i + 3, i + 4).equals("e")){
+        result = result + 1;
+      }
+      
+    }
+    return result;
+  }
+  /**
+   * Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+   * @param str
+   * @return
+   */
+  public int bobThere(String str){
+    int result = 0;
+
+    for (int i = 0; i < str.length() - 2; i++){
+      if (str.substring(i , i + 1).equals("b") && str.substring(i + 2, i + 3).equals("b")){
+        result = result + 1;
       }
     }
     return result;
   }
-}
+  /**
+   * Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+   * @param str
+   * @param n
+   * @return
+   */
+  public String repeatEnd(String str , int n){
+    String result = "";
+
+    for (int i = 0; i < n; i++){
+      String lastC = str.substring(str.length() - n);
+      result = result + lastC;
+    }
+    return result;
+} 
+  /**
+   * 
+   * @param str
+   * @param n
+   * @return
+   */
+  public boolean prefixAgain(String str , int n){
+    String temp = str.substring(0 , n);
+    String remaining = str.substring(n);
+
+    if (remaining.indexOf(temp) >= 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+    
+
+    }
+    /**
+     * Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the star, they are the same.
+     * @param str
+     * @return
+     */
+  public boolean sameStarChar(String str){
+
+    for (int i = 1; i < str.length() - 1; i++){
+      if (str.substring(i , i + 1).equals("*") && !str.substring(i - 1 , i).equals(str.substring(i + 1 , i + 2))){
+        return false;
+      }
+    }
+    return true;
+  }
+  /**
+   * ask ms chen tmr
+   * @param str
+   * @return
+   */
+  public String starOut(String str){
+    
+    String result = "";
+    int k = str.indexOf("*");
+    String first = str.substring( 0 , k - 1);
+    String second = str.substring( k + 2);
+    result = first + second;
+
+    return result;
+    
+  }
+  /**
+   * Return the number of times that the string "hi" appears anywhere in the given string.
+   * @param str
+   * @return
+   */
+  public int countHi(String str){
+    int result = 0;
+
+    for (int i = 0; i < str.length() - 1; i++){
+      if (str.substring(i , i + 2).equals("hi")){
+        result++;
+      }
+    }
+    return result;
+  } 
+  public String endOther(String str, String sstr){
+    
+  }
+  }
+
+
+  
