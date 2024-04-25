52a72d6a5c6c5a86cb72c766a995261d3421ad0c
penre <tianyou.zhang25@ycdsbk12.ca>
2024-04-25 14:30:08 -0400

    do more

--- Main.java
+++ Main.java
@@ -1,8 +1,33 @@
 class Main extends ConsoleProgram {
   public void run() {
-      // Make test calls to your methods here
+    System.out.print(countCode("codexxcode"));
   }
+  /**
+   * make a program that repeats everyletter in a string 
+   * @param str
+   * @return
+   */
+  public String doubleChar(String str){
+    String result = "";
 
-  // Define your methods and and add javadocs for each one:
+    for (int i = 0; i < str.length(); i++){
+      char c = str.charAt(i);
+      result += Character.toString(c) + Character.toString(c);
 
+    }
+    return result;
+  }
+  
+
+  public int countCode(String str){
+    int result = 0;
+    int count;
+
+    for (int i = 0; i < str.length(); i++){
+      for (count = str.indexOf("code"); count < str.length(); count++){
+        result += count;
+      }
+    }
+    return result;
+  }
 }
